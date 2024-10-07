cf253d996a1013aa301cee67f3b2cbebfca24d3b
Meboy1000 <Meboy10000@gmail.com>
2024-10-07 10:37:19 -0700

    bottles r,g,b potions, no true logic

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
-
+from .utils import inv
 
 router = APIRouter(
     prefix="/bottler",
@@ -21,20 +21,9 @@ class PotionInventory(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
-    with db.engine.begin() as connection:
-        inventory = connection.execute(sqlalchemy.text("SELECT num_green_potions, num_green_ml FROM global_inventory")).first()
-        newpots = inventory[0]
-        new_green_ml = inventory[1]
-        #new_red_ml = 0
-        #new_blue_ml = 0
-        #new_dark_ml = 0
-        for PotionInv in potions_delivered:
-            newpots += PotionInv.quantity
-            #new_red_ml -= PotionInv.quantity * PotionInv.potion_type[0] 
-            new_green_ml -= PotionInv.quantity * PotionInv.potion_type[1]
-            #new_blue_ml -= PotionInv.quantity * PotionInv.potion_type[2]
-            #new_dark_ml -= PotionInv.quantity * PotionInv.potion_type[3]
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_potions = {newpots}, num_green_ml = {new_green_ml}"))
+    for potions in potions_delivered:
+        potion = inv.get_potions_type(potions.potion_type)
+        inv.update_potions(potion.sku, potions.quantity)
     print(f"potions delievered, order_id: {order_id}")
     
     return "OK"
@@ -48,19 +37,30 @@ def get_bottle_plan():
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
-    with db.engine.begin() as connection:
-        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory")).first()
-    # Updated logic: bottle all barrels into green potions. Do not bottle if out of ml
-        ml = inventory[3]
-        quantity = ml//100
-        if quantity > 0:
-            return [
-                    {
-                        "potion_type": [0, 100, 0, 0],
-                        "quantity": (quantity),
-                    }
-                ]
-        return[]
+    plan = []
+    catalog = inv.get_potions_catalog()
+    ml_inv = inv.get_ml()
+    for potion in catalog:
+        quantity = inv.get_potion_cap()
+        # temp logic, for pure color potions
+        check = False
+        for x in range(4):
+            try:
+                quantity = min(quantity, ml_inv[x]//potion.recipe[x])
+                check = True
+            except ZeroDivisionError:
+                continue
+        
+        if quantity != 0 and check:
+            plan.append([
+                {
+                    "potion_type" : potion.recipe,
+                    "quantity" : quantity,
+                }
+            ])
+
+
+    return plan
 
 if __name__ == "__main__":
     print(get_bottle_plan())
