37cf595b82c648697dddd134f115519b5a6a93e3
Meboy1000 <Meboy10000@gmail.com>
2024-10-13 02:09:39 -0700

    fixed 0 quantity problem

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -88,10 +88,11 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         for barrel in catalog[x]:
             if barrel.price != 0:
                 affordable = int(min(budget[x] // barrel.price, barrel.quantity, needed[x]//barrel.ml_per_barrel))
-                plan.append({
-                    "sku": barrel.sku,
-                    "quantity": affordable,
-                })
+                if affordable != 0:
+                    plan.append({
+                        "sku": barrel.sku,
+                        "quantity": affordable,
+                    })
                 budget[x] -= barrel.price*affordable
                 needed[x] -= barrel.ml_per_barrel*affordable
         budget[x+1] += budget[x]
